3092ab519a0c41d139e3be36bc9d03ca6ca12985
LucasHubnerDev <hubner.dev@gmail.com>
2026-05-12 21:24:44 -0300

    Adicionar arquivos de projeto.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,15 @@
+﻿// Screen Sound
+
+// Declarando uma variável do tipo string para armazenar a mensagem de boas-vindas
+// A variável é do tipo string, o que significa que ela pode armazenar uma sequência de caracteres (texto)
+// Toda declaração de variável em C# deve incluir o tipo da variável (neste caso, string), o nome da variável (messageWelcome) e o valor inicial (a mensagem de boas-vindas)
+string messageWelcome = "Boas vindas ao Screen Sound!";
+
+// Posso referenciar a variável messageWelcome em qualquer parte do código após a sua declaração, e ela conterá o valor "Boas vindas ao Screen Sound!"
+Console.WriteLine(messageWelcome); // Imprime a mensagem de boas-vindas no console
+
+
+string nome = "Rodrigo";
+string sobrenome = "Santos";
+
+Console.WriteLine($"Olá, {nome} {sobrenome}! Bem-vindo ao Screen Sound!"); // Imprime uma mensagem personalizada usando interpolação de strings
